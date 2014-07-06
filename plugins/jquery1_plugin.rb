81701746130bc0794e523f4d3a73e117e38a8a77
Alberto A. Colon Viera (Albertico) <aacv@alberti.co>
2014-07-06 15:53:16 -0400

    jQuery v1.x plugin

---
+++ plugins/jquery1_plugin.rb
@@ -0,0 +1,6 @@
+##
+# Plugin for installing jQuery 1.x into Padrino project.
+#
+# Javascript files.
+get 'http://code.jquery.com/jquery-1.11.1.js', destination_root('public/javascripts/jquery-1.11.1.js')
+get 'http://code.jquery.com/jquery-1.11.1.min.js', destination_root('public/javascripts/jquery-1.11.1.min.js')

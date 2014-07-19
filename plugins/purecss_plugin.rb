304593f8cf78159fa833e3ee4c78a967ffd8673a
Alberto A. Colon Viera (Albertico) <aacv@alberti.co>
2014-07-19 17:21:02 -0400

    Pure CSS plugin (Responsive CSS modules)

---
+++ plugins/purecss_plugin.rb
@@ -0,0 +1,6 @@
+##
+# Plugin for installing Pure CSS into any Padrino project.
+#
+# CSS Stylesheets
+get 'http://yui.yahooapis.com/pure/0.5.0/pure.css', destination_root('public/stylesheets/pure.css')
+get 'http://yui.yahooapis.com/pure/0.5.0/pure-min.css', destination_root('public/stylesheets/pure-min.css')

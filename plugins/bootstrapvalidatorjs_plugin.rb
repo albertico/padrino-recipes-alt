c26c930669010517af0b168f2f0c83a59fc8ec01
Alberto A. Colon Viera (Albertico) <aacv@alberti.co>
2014-08-25 18:36:47 -0400

    BootstrapValidator plugin updated to v0.5.1

--- plugins/bootstrapvalidatorjs_plugin.rb
+++ plugins/bootstrapvalidatorjs_plugin.rb
@@ -3,8 +3,8 @@
 # Must also install the jQuery and Bootstrap libraries and include them in your project views.
 #
 # Javascript libraries.
-get 'https://raw.github.com/nghuuphuoc/bootstrapvalidator/v0.4.5/dist/js/bootstrapValidator.js', destination_root('public/javascripts/bootstrapValidator.js')
-get 'https://raw.github.com/nghuuphuoc/bootstrapvalidator/v0.4.5/dist/js/bootstrapValidator.min.js', destination_root('public/javascripts/bootstrapValidator.min.js')
+get 'https://raw.github.com/nghuuphuoc/bootstrapvalidator/v0.5.1/dist/js/bootstrapValidator.js', destination_root('public/javascripts/bootstrapValidator.js')
+get 'https://raw.github.com/nghuuphuoc/bootstrapvalidator/v0.5.1/dist/js/bootstrapValidator.min.js', destination_root('public/javascripts/bootstrapValidator.min.js')
 # CSS Stylesheets.
-get 'https://raw.github.com/nghuuphuoc/bootstrapvalidator/v0.4.5/dist/css/bootstrapValidator.css', destination_root('public/stylesheets/bootstrapValidator.css')
-get 'https://raw.github.com/nghuuphuoc/bootstrapvalidator/v0.4.5/dist/css/bootstrapValidator.min.css', destination_root('public/stylesheets/bootstrapValidator.min.css')
+get 'https://raw.github.com/nghuuphuoc/bootstrapvalidator/v0.5.1/dist/css/bootstrapValidator.css', destination_root('public/stylesheets/bootstrapValidator.css')
+get 'https://raw.github.com/nghuuphuoc/bootstrapvalidator/v0.5.1/dist/css/bootstrapValidator.min.css', destination_root('public/stylesheets/bootstrapValidator.min.css')
